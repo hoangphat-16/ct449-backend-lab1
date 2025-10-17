94067083023f3660fdce7f34b7eade9d04bdcc7f
hoangphat-16 <phatb2008317@student.ctu.edu.vn>
2025-10-17 19:00:42 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routers/contact.router.js");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -10,6 +11,22 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+
 app.use("/api/contacts", contactsRouter);
+
+
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+
+app.use((err, req, res, next) => {
+    
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error"
+    });
+});
+
 
 module.exports = app;
